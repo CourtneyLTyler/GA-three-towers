8429bc2f3478f1743826c526aaf678b05034e10b
Courtney Tyler <courtneytyler7@gmail.com>
2019-01-08 12:01:08 -0500

    more logic

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ let rod1 = []
 let rod2 = []
 let rod3 = []
 
-// grab rods and store in variables
-const rodObj1 = document.querySelector('#rod-1')
-const rodObj2 = document.querySelector('#rod-2')
-const rodObj3 = document.querySelector('#rod-3')
-
 // grab disks and store in variables
 const diskSm1 = document.querySelector('#small-disk')
 const diskMd2 = document.querySelector('#med-disk')
 const diskLg3 = document.querySelector('#lg-disk')
+
+// grab rods and store in variables
+const rodObj1 = document.querySelector('#rod-1')
+const rodObj2 = document.querySelector('#rod-2')
+const rodObj3 = document.querySelector('#rod-3')
 
 // array for disks in play
 let disksInPlay = []
@@ -22,37 +22,41 @@ let gameStart = () => {
     disksInPlay = []
     rod2 = []
     rod3 = []
-    // Add this text to 'whoseTurn'
-    document.querySelector("#ready").innerHTML = "Ready to play? Click on the top disk!"
+    // Add this text to 'message'
+    document.querySelector("#message").innerHTML = "Ready to play? Click on the top disk!"
 }
 
-// test gameStart
 gameStart()
+// test gameStart
 console.log(rod1) 
-
 
 // get reset button, store in 'resetButton'
 let resetButton = document.querySelector(".reset")
-// add the reset fx to the reset button
+
+// add the gameStart fx to the reset button
 resetButton.addEventListener("click", gameStart)
 
-
-// create event listener for the first click on a disk, that would execute the following
-// 
+// create fx for when a disk is clicked
 let firstClick = () => {
     // store the current disk's data-id in 'diskId'
-	let diskId = this.getAttribute("data-id");
+	let diskId = event.target.getAttribute("data-id")
     // create copy of the clicked disk
     let diskCopy = diskId
     // code to move the clicked disk copy to the inPlay array
     disksInPlay.push(diskCopy)
-    alert("Where would you like to move your disk? Click on the rod")
+    // Add this text to 'message'
+    document.querySelector("#message").innerHTML = "Where would you like to move your disk? Click on the rod"
 }
+
+// add listen to clicks to the disks, execute 1st click fx
+diskSm1.addEventListener('click', firstClick)
+diskMd2.addEventListener('click', firstClick)
+diskLg3.addEventListener('click', firstClick)
 
 // create event listener for the second click on the rod, that would execute the following
 let secondClick = () => {
     // store the current rod's id in 'rodId'
-	let rodId = this.getAttribute("id");
+	let rodId = event.target.getAttribute("id");
     // if the current rod array is empty, 
     if (rodId = []) {
         // push disksInPlay[0] to the selected rod
@@ -73,7 +77,8 @@ let secondClick = () => {
             // check for a win
             checkForWin()
         } else {
-            alert("Illegal move, please try again")
+            // Add this text to 'message'
+            document.querySelector("#message").innerHTML = "Illegal move, please try again"
             // clear disksInPlay
             disksInPlay = []
         }
@@ -81,9 +86,17 @@ let secondClick = () => {
     }
 }
 
+// add listen to click to the rods, execute 2nd click fx
+rodObj1.addEventListener('click', secondClick)
+rodObj2.addEventListener('click', secondClick)
+rodObj3.addEventListener('click', secondClick)
+
 // create logic to check for win
 let checkForWin = () => {
     if (rod3 === [3,2,1]) {
-        alert("Congratulations, you have won the game!  Hit reset to play again!")
+        // Add this text to 'message'
+        document.querySelector("#message").innerHTML = "Congratulations, you have won the game!  Hit reset to play again!"
+        // clear disksInPlay
+        disksInPlay = []
     }
 }
