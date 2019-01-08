aacc47a3289bec6940533d4d780306ee15f9499a
Courtney Tyler <courtneytyler7@gmail.com>
2019-01-08 11:01:13 -0500

    add more secondClick logic

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ let rod2 = []
 let rod3 = []
 
 // Create disks as numbers
-let disk1sm = 1
-let disk2md = 2
-let disk3lg = 3
+const disk1sm = 1
+const disk2md = 2
+const disk3lg = 3
 
 // array for disks in play
 let disksInPlay = []
@@ -24,11 +24,44 @@ let gameStart = () => {
 gameStart()
 console.log(rod1) // logs [3,2,1]
 
-// create event listener for clicking on a disk, that would execute the following
+// create event listener for the first click on a disk, that would execute the following
+// 
 let firstClick = () => {
-    // code to move the clicked disk to the inPlay array
-    disksInPlay.push(event.target)
+    // store the current disk's data-id in 'diskId'
+	let diskId = this.getAttribute("data-id");
+    // create copy of the clicked disk
+    let diskCopy = diskId
+    // code to move the clicked disk copy to the inPlay array
+    disksInPlay.push(diskCopy)
     alert("Where would you like to move your disk? Click on the rod")
 }
 
-// once rod is clicked, 
+// create event listener for the second click on the rod, that would execute the following
+let secondClick = () => {
+    // store the current rod's id in 'rodId'
+	let rodId = this.getAttribute("id");
+    // if the current rod array is empty, 
+    if (rodId = []) {
+        // push disksInPlay[0] to the selected rod
+        rodId.push(disksInPlay[0])
+    // otherwise, 
+    } else if {
+        // make a copy of the last number of the selected rod array, then
+        let copyOfLast = rodId[(rodId.length)-1]
+        // push that copy to disksInPlay, then
+        disksInPlay.push(copyOfLast)
+        // compare values
+        if (disksInPlay[0] < disksInPlay[1]) {
+            // push the disk from firstClick to the rod array selected in secondClick, then
+            rodId.push(diskId)
+            // clear disksInPlay
+            disksInPlay = []
+        } else {
+            alert("Illegal move, please try again")
+            // clear disksInPlay
+            disksInPlay = []
+        }
+        
+    }
+}
+
