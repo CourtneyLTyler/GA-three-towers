f5c5d5d29e031c8186d5e47604fc7fa3bd4ad822
Courtney Tyler <courtneytyler7@gmail.com>
2019-01-11 09:23:23 -0500

    clean code further

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-// Create rod arrays
 let rod1 = []
 let rod2 = []
 let rod3 = []
 
-// grab disks and store in variables
+// grab disk objects
 const diskSm1 = document.querySelector('#a')
 const diskMd2 = document.querySelector('#b')
 const diskLg3 = document.querySelector('#c')
@@ -13,20 +12,18 @@ const diskId1 = diskSm1.getAttribute("id")
 const diskId2 = diskMd2.getAttribute("id")
 const diskId3 = diskLg3.getAttribute("id")
 
-// grab rodcols and store in variables
+// grab rodcol objects
 const rodObj1 = document.querySelector('#stick1')
 const rodObj2 = document.querySelector('#stick2')
 const rodObj3 = document.querySelector('#stick3')
 
-// array for disks in play
 let disksInPlay = []
 
-// function to start or restart the game
 let gameStart = () => {
     rod1.length = 0
     // add the Ids to rod1 array
     rod1.push(diskId3, diskId2, diskId1)
-    // position the disks in rodcol1
+    // position the html - disks in rodcol1
     let startParent = document.getElementById('rodcol1')
     startParent.appendChild(diskLg3)
     startParent.appendChild(diskMd2)
@@ -34,7 +31,6 @@ let gameStart = () => {
     disksInPlay.length = 0
     rod2.length = 0
     rod3.length = 0
-    // Add this text to 'message'
     document.querySelector("#message").innerHTML = "Ready to play? Click on the top disk!"
     diskSm1.addEventListener('click', diskClick)
     disableRodClick()
@@ -42,26 +38,22 @@ let gameStart = () => {
 
 gameStart()
 
-// get reset button, store in 'resetButton'
 let resetButton = document.querySelector(".reset")
-// add the gameStart fx to the reset button
 resetButton.addEventListener("click", gameStart)
 
-// add listener clicks to the disks, execute diskClick fx
+// add listener clicks to the disk objects
 function enableDiskClick() {
     diskSm1.addEventListener('click', diskClick)
     diskMd2.addEventListener('click', diskClick)
     diskLg3.addEventListener('click', diskClick)
 }
 
-// declare fx to disable the disk clicks
 function disableDiskClick() {
     diskSm1.removeEventListener("click", diskClick)
     diskMd2.removeEventListener("click", diskClick)
     diskLg3.removeEventListener("click", diskClick)
 }
 
-// add listen for click to the rods, execute 2nd click fx
 function enableRodClick() {
     rodObj1.addEventListener('click', rodClick)
     rodObj2.addEventListener('click', rodClick)
@@ -74,118 +66,11 @@ function disableRodClick() {
     rodObj3.removeEventListener("click", rodClick)
 }
 
-// // make variables global
-// // let count = 0 
-// let clearTime
-// let seconds = 0 
-// let minutes = 0 
-// let hours = 0
-// // let clearState
-// let secs = '0' + seconds 
-// console.log("this should be secs " + secs)
-// let mins = '0' + minutes + ':' 
-// console.log("this should be mins " + mins)
-// let gethours = '0' + hours + ':'
-// console.log("this should be hours " + gethours)
-// let fulltime = document.getElementById('fulltime')
-// console.log("should be obj " + fulltime)
-// let gameTimerObj = document.getElementById("gameTimer") 
-// console.log("should be obj " + gameTimerObj)
-// let startBtn = document.getElementById('start-timer')
-// console.log("should be obj " + startBtn)
-// startBtn.addEventListener('click', startTime)
-// // gameTimerObj.innerHTML = gethours + mins + secs
-
-// let stopBtn = document.getElementById('stop-timer')
-// stopBtn.addEventListener('click', stopTime)
-
-// function startWatch() { 
-//     // if seconds is 60 add 1 to minutes, and reset seconds
-//     if (seconds === 60) { 
-//         seconds = 0 
-//         minutes = minutes + 1
-//     } 
-//     // if there are <10 minutes, display a zero before the minute 
-//     if (mins = (minutes<10)) {
-//         mins = '0'+ minutes + ':'
-//     } else {
-//         mins = minutes + ':'
-//     }
-//     //  if minutes is 60, add 1 to hours and reset minutes
-//     if (minutes === 60) { 
-//         minutes = 0 
-//         hours = hours + 1 
-//     } 
-//     // if hours is <10, display a zero before the hour
-//     if (gethours = (hours<10)) {
-//         gethours = '0' + hours + ':'
-//     } else {
-//         gethours = hours + ':'
-//     }
-
-//     if (secs = (seconds<10)) {
-//         secs = '0' + seconds
-//     } else {
-//         secs = seconds
-//     }
-
-//     let displayTheDamnTimer = gethours + mins + secs
-//     gameTimerObj.innerHTML = displayTheDamnTimer
-//     // start adding to seconds
-//     seconds++ 
-//     // setTimeout keeps it going and calls it every second
-//     let clearTime = setTimeout(startWatch, 1000)
-//     console.log(clearTime)
-// } 
-
-// // setTimeout ({
-// //     startWatch() 
-// //   }, 1000)
-
-// //   setTimeout(() => {
-// //     startWatch()
-// //   }, 1000)
-
-// function startTime() { 
-//     fulltime.style.display = "block" 
-//     // hide start button while timer is running
-//     this.style.display = "none" 
-//     // make it go
-//     startWatch() 
-// }
-
-// //create a function to stop the time 
-// function stopTime() { 
-//     //display the full time 
-//     fulltime.style.display = "block"
-//     let time = gethours + mins + secs
-//     fulltime.innerHTML = 'Your time: ' + time
-//     // reset the timer
-//     seconds = 0
-//     minutes = 0 
-//     hours = 0 
-//     secs = '0' + seconds 
-//     mins = '0' + minutes + ': ' 
-//     gethours = '0' + hours + ': '
-//     /* display the stopwatch after it's been stopped */ 
-//     var x = document.getElementById ("gameTimer"); 
-//     // let stoppedTime = gethours + mins + secs; 
-//     // gameTimerObj.innerHTML = stoppedTime 
-//     var stopTime = gethours + mins + secs; 
-//     x.innerHTML = stopTime
-//     // show start button
-//     startBtn.style.display = "inline-block" 
-//     /* clear the stop watch using the setTimeout() return value 'clearTime' as ID */ 
-//     clearTimeout(clearTime)    
-// } 
-
-// create logic to check for win
 function checkForWin() {
     if (rod3.length ===3) {
         document.querySelector("#message").innerHTML = "Congratulations, you have won the game!  Hit reset to play again!"   
         disableDiskClick()
         disableRodClick()
-        // stopTime()
     }
 }
 
@@ -223,7 +108,7 @@ function diskClick() {
     function restOfFunction() {
         // add the current diskId to disksInPlay array
         disksInPlay.push(diskId)
-        // make the parent of the clicked disk's id global
+        // push the parent of the clicked disk's id to rodEval to make it global
         rodEval.push(stackedOnId)
         document.querySelector("#message").innerHTML = "Where would you like to move your disk? Click on the rod"
         enableRodClick()
@@ -235,7 +120,6 @@ function rodClick() {
     // store the clicked sticks's id 
     let rodId = event.target.getAttribute("id")
     // find the array that corresponds with the clicked rod
-    console.log("should be event target " + event.currentTarget + "also, rodClick fx has run")
     if(rodId == 'stick1') {
         // grab the corresponding parent obj 
         let parent1 = document.querySelector('#rodcol1')
@@ -256,7 +140,7 @@ function rodClick() {
             rodx.push(disksInPlay[0])
             // store the first item of the disksInPlay array
             let currentDiskObj = document.getElementById(disksInPlay[0])
-            // move the current disk to the correct column
+            // move it to the correct column
             parent.appendChild(currentDiskObj)
             // remove the disk from its prior array
             if(rodEval == 'rodcol1') {
@@ -270,7 +154,6 @@ function rodClick() {
                 rod3.pop()   
                 rodEval.length = 0
             }
-            
             document.querySelector("#message").innerHTML = "Click on the next disk to move!"
             disksInPlay.length = 0
             enableDiskClick()
@@ -280,14 +163,13 @@ function rodClick() {
             let copyOfLast = rodx[((rodx.length)-1)]
             // push that copy to disksInPlay, then
             disksInPlay.push(copyOfLast)
-            // is move legal? 
-            // if the first item of disksInPlay array is < the second item,
+            // is move legal? if the first item of disksInPlay array is < the second item,
             if (disksInPlay[0] < disksInPlay[1]) {
                 // push the first disk from disksInPlay to the corresponding rod array
                 rodx.push(disksInPlay[0])
                 // grab the diskInPlay object
                 let smallerDisk = document.getElementById(disksInPlay[0])
-                // move the disk to the rod
+                // move the html - disk to the rod
                 parent.appendChild(smallerDisk)
                 // remove the disk from its prior array
                 if(rodEval == 'rodcol1') {
@@ -313,6 +195,5 @@ function rodClick() {
                 enableDiskClick()
             } 
         }
-    console.log("end of turn rod1, rod2, rod3 " + rod1 + " " + rod2 + " " + rod3)    
     }
 }
